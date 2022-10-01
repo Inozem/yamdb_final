38f1ca843a452278fed5bc4263bb7bc6dad107a7
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2022-10-01 17:49:20 +0300

    Changed ALLOWED_HOSTS

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='ALLOWED_HOSTS').split()
 
 MAIL = 'from@example.com'
 
